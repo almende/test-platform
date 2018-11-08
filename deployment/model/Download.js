438d9c62ccbca92c0e65887cd0d023b3ae13cf91
Ludo Stellingwerff <ludo@almende.org>
2018-11-08 17:13:33 +0100

    More bugs...

--- deployment/model/Download.js
+++ deployment/model/Download.js
@@ -3,6 +3,7 @@
 const downloader = require('download')
 const fs = require('fs-extra')
 const exec = require('child_process').exec
+const http = require('http')
 
 class Download {
   constructor (id, url) {
@@ -12,7 +13,6 @@ class Download {
     this.progress = {}
 
     this.updateStatus()
-    this.proceed()
     setInterval(this.proceed.bind(this), 2000)
   };
 
@@ -22,7 +22,7 @@ class Download {
       case 'Initial':
         setTimeout(this.download.bind(this), 0)
         break
-      case 'Downloaded':
+      case 'Installable':
         setTimeout(this.install.bind(this), 0)
         break
       default:
@@ -30,14 +30,29 @@ class Download {
     }
   }
 
-  updateStatus () {
+  testRegistry () {
+    let me = this
+    return new Promise((resolve, reject) => {
+      http.request('http://registry:5000/v2/' + me.id + '/manifests/latest', { method: 'HEAD' }, (res) => {
+        resolve(res.statusCode === 200)
+      }).on('error', (e) => { reject(e) })
+    })
+  }
+
+  async updateStatus () {
     // Check status of files on fs and in register
     if (fs.existsSync('/usr/src/app/downloads/' + this.id + '.download.zip')) {
       this.status = 'Downloaded'
     }
-    if (fs.existsSync('/usr/src/app/downloads/' + this.id + '.download.zip_unpacked') && fs.existsSync('/usr/src/app/downloads/' + this.id + '.download.zip_unpacked/manifest.json')) {
-      this.status = 'Done' // Just assume that the actual push is done
-    }
+    try {
+      if (await this.testRegistry()) {
+        this.status = 'Done'
+      } else {
+        this.status = 'Installable'
+      }
+    } catch (err) { console.error('failed to check registry', err) }
+
+    setTimeout(this.proceed.bind(this), 0)
   }
 
   download () {
@@ -56,7 +71,7 @@ class Download {
           me.errorReport = { error: error, body: body, response: response }
         })
         .then(() => {
-          me.status = 'Downloaded'
+          me.status = 'Installable'
         })
     }
   }
--- executionManager/server.js
+++ executionManager/server.js
@@ -1,18 +1,17 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const server = new express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = new express()
 
 // register JSON parser middlewear
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
-require('./routes/assetRoutes')(server);
+require('./routes/assetRoutes')(server)
 
 server.listen(9000, () => {
 
-    /* eslint-disable */
-    console.log('Server is up!');
-});
+  /* eslint-disable */
+  console.log('Server is up!')
+})
