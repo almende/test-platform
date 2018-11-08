cdb713d277faacf87353277b8fab555b03ee7b70
Ludo Stellingwerff <ludo@almende.org>
2018-11-08 12:05:52 +0100

    Add new image cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+docker image rm vfos/deploy
+docker image rm localhost:5000/vfos/deploy
+docker image rm vfos/exec-manager
+docker image rm localhost:5000/vfos/exec-manager
+docker image rm vfos/aim
+docker image rm localhost:5000/vfos/aim
+docker image rm vfos/system-dashboard
+docker image rm localhost:5000/vfos/system-dashboard
+docker image rm vfos/portal
+docker image rm localhost:5000/vfos/portal
+docker image rm vfos/test-server
+docker image rm localhost:5000/vfos/test-server
+
