797b5d41a5ac45ca4a513e1356647c51dd30e98a
Ludo Stellingwerff <ludo@almende.org>
2019-02-11 16:40:04 +0100

    Add config web form

--- esp_storage/static/command.js
+++ esp_storage/static/command.js
@@ -4,8 +4,8 @@ function postConfig (config) {
   xhr.open('POST', '/config/')
   xhr.setRequestHeader('Content-Type', 'application/json')
   xhr.onload = function () {
-    if (xhr.status === 200 && xhr.responseText !== 'ok') {
-      alert('Something went wrong.  Response is:' + xhr.responseText)
+    if (xhr.status === 200) {
+      location.reload()
     }
     else if (xhr.status !== 200) {
       alert('Request failed.  Returned status of ' + xhr.status)
