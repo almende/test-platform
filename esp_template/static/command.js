574f05d2d4ab181b0e449ef678e6d388952fec85
Ludo Stellingwerff <ludo@almende.org>
2019-04-10 16:29:03 +0200

    ESP template updates

---
+++ esp_template/static/command.js
@@ -0,0 +1,15 @@
+function postConfig (config) {
+  var xhr = new XMLHttpRequest()
+
+  xhr.open('POST', 'config/')
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      location.reload()
+    }
+    else if (xhr.status !== 200) {
+      alert('Request failed.  Returned status of ' + xhr.status)
+    }
+  }
+  xhr.send(JSON.stringify(config))
+}
