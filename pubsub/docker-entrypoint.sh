54c8752265ae8e7ae508ed17e7acdc0caeff9871
Ricardo Grim Cabrita <grim@uninova.pt>
2019-05-30 17:14:31 +0100

    Include the creation of the default exchanges in the Messaging/PubSub deployment

--- pubsub/docker-entrypoint.sh
+++ pubsub/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 cat ./vfos_messaging_pubsub_config.config > /config/config
 cat ./vfos_messaging_pubsub_plugins.config > /config/plugins
-
+cat ./vfos_messaging_pubsub_definitions.json > /config/definitions
 
 exec /usr/bin/dumb-init -c "$@"
